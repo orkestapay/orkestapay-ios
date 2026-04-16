20d17e2586a8dacef8bda4bba593c5693b1be41c
Hector Rodriguez <hector.rodriguez@zenki.fi>
2026-04-16 12:08:58 -0600

    change

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Orkestapay",
-            path: "Sources",
-            exclude: ["Resources/PrivacyInfo.xcprivacy"],
+            path: "Sources/Orkestapay",
             resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
         ),
     ]
